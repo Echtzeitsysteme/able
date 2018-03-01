276d5f5a084ae120c56c40decb7890fa3a212650
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-03-01 12:56:20 +0100

    Add suggestion for how to represent the UUIDs as constants

--- android/Application/src/main/java/de/tudarmstadt/es/able/CapLedConstants.java
+++ android/Application/src/main/java/de/tudarmstadt/es/able/CapLedConstants.java
@@ -1,5 +1,7 @@
 package de.tudarmstadt.es.able;
 
+import java.util.UUID;
+
 /**
  * Created by user on 01.03.18.
  */
@@ -11,7 +13,9 @@ public interface CapLedConstants {
     final static String ledCharacteristicUUID = ("00000000-0000-1000-8000-00805f9b34f1");
     final static String capsenseCharacteristicUUID = ("00000000-0000-1000-8000-00805f9b34f2");
 
-
-
+    UUID BASE_UUID = UUID.fromString(baseUUID);
+    UUID CAPLED_SERVICE_UUID = UUID.fromString(BASE_UUID.toString() + "0");
+    UUID CAPLED_LED_CHARACTERISTIC_UUID = UUID.fromString(BASE_UUID.toString() + "1");
+    UUID CAPLED_CAP_CHARACTERISTIC_UUID = UUID.fromString(BASE_UUID.toString() + "2");
 
 }
