7041d0a12cba0d446225f13365f3877c607b51b9
Puria Izady <puria@Purias-MBP.fritz.box>
2018-08-19 21:29:08 +0200

    read sensor value from fm4

--- android/Application/src/main/java/de/tudarmstadt/es/cppp/CPPPSettingsTab.java
+++ android/Application/src/main/java/de/tudarmstadt/es/cppp/CPPPSettingsTab.java
@@ -6,6 +6,7 @@ import android.bluetooth.BluetoothGattService;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -161,6 +162,7 @@ public class CPPPSettingsTab extends Fragment implements BLEServiceListener {
     public void dataAvailable(Intent intent) {
 
     }
+
 
     /**
      * Sets the GUI ScanButton.
--- android/Application/src/main/java/de/tudarmstadt/es/cppp/CPPPViewTab.java
+++ android/Application/src/main/java/de/tudarmstadt/es/cppp/CPPPViewTab.java
@@ -22,9 +22,11 @@ import de.tudarmstadt.es.able.BLEBroadcastReceiver;
 import de.tudarmstadt.es.able.DeviceScanActivity;
 import de.tudarmstadt.es.able.R;
 import de.tudarmstadt.es.able.BLEServiceListener;
+import de.tudarmstadt.es.capled.CapLEDConstants;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class CPPPViewTab extends Fragment implements BLEServiceListener  {
@@ -37,7 +39,12 @@ public class CPPPViewTab extends Fragment implements BLEServiceListener  {
     BLEBroadcastReceiver thisReceiver;
 
     public static BluetoothGattCharacteristic CPPP_JOYSTICK_1_CHARACTERISTIC;
+    private static BluetoothGattDescriptor CPPP_JOYSTICK_NOTIFCATION;
+
     private static TextView CPPP_JOYSTICK_1_X_VALUE;
+    private static String Joystick1XValue = "-1";
+    private static Switch notify_switch;
+    private static boolean notifyState = false;
 
     public static CPPPViewTab newInstance(String mDeviceName, String mDeviceAddress) {
         Bundle args = new Bundle();
@@ -61,6 +68,15 @@ public class CPPPViewTab extends Fragment implements BLEServiceListener  {
         Activity act = getActivity();
         mDeviceAddress = getArguments().getString("mDeviceAddress");
         CPPP_JOYSTICK_1_X_VALUE = rootView.findViewById(R.id.CPPP_JOYSTICK_1_X_VALUE);
+        notify_switch = rootView.findViewById(R.id.cpppNotifySwitch);
+
+        notify_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                writeNotification(isChecked);
+                notifyState = isChecked;
+            }
+        });
+
         return rootView;
     }
 
@@ -115,12 +131,42 @@ public class CPPPViewTab extends Fragment implements BLEServiceListener  {
     public void gattServicesDiscovered() {
         BluetoothGattService mService = BluetoothLeService.mBluetoothGatt.getService(CPPPConstants.CPPP_SERVICE_UUID);
         CPPP_JOYSTICK_1_CHARACTERISTIC = mService.getCharacteristic(CPPPConstants.CPPP_JOYSTICK_1_CHARACTERISTIC_UUID);
+        CPPP_JOYSTICK_NOTIFCATION = CPPP_JOYSTICK_1_CHARACTERISTIC.getDescriptor(CPPPConstants.CPPP_NOTIFICATION);
+        notify_switch.setEnabled(true);
 
+        readJoystick1Characteristic();
 
     }
 
     @Override
     public void dataAvailable(Intent intent) {
+        Joystick1XValue = "-1";
+        String uuid = BluetoothLeService.getmCharacteristicToPass().getUuid().toString();
+        if (uuid.equals(CPPPConstants.CPPP_JOYSTICK_1_CHARACTERISTIC_UUID.toString())) {
+            Joystick1XValue = BluetoothLeService.getmCharacteristicToPass().getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 0).toString();
+        }
+        CPPP_JOYSTICK_1_X_VALUE.setText(Joystick1XValue);
+    }
 
+    public void writeNotification(boolean value) {
+        BluetoothLeService.mBluetoothGatt.setCharacteristicNotification(CPPP_JOYSTICK_1_CHARACTERISTIC, value);
+        byte[] byteVal = new byte[1];
+        if (value) {
+            byteVal[0] = 1;
+        } else {
+            byteVal[0] = 0;
+        }
+        Log.i(TAG, "CapSense Notification " + value);
+        CPPP_JOYSTICK_NOTIFCATION.setValue(byteVal);
+        BluetoothLeService.mBluetoothGatt.writeDescriptor(CPPP_JOYSTICK_NOTIFCATION);
+    }
+
+    public void readJoystick1Characteristic() {
+        if (BluetoothLeService.existBluetoothAdapter() == false ||
+                BluetoothLeService.existBluetoothGatt() == false) {
+            Log.w(TAG, "BluetoothAdapter not initialized");
+            return;
+        }
+        BluetoothLeService.genericReadCharacteristic(CPPP_JOYSTICK_1_CHARACTERISTIC);
     }
 }
