76fd44e7e045c994e618902236245bc60871bd9e
Puria Izady <puria@Purias-MBP.fritz.box>
2018-09-21 17:55:45 +0200

    minor

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -2,11 +2,11 @@
 
 echo Starting SED deactivation of CAPLED in ANDROID MANIFEST ...
 
-sed 's/\<activity android:name=\"de.tudarmstadt.es.capled.CapLEDActivity\" \/\>/\<!--\<activity android:name=\"de.tudarmstadt.es.capled.CapLEDActivity\" \/\>--\>/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
+#sed 's/\<activity android:name=\"org.able.capled.CapLEDActivity\" \/\>/\<!--\<activity android:name=\"org.able.capled.CapLEDActivity\" \/\>--\>/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
 
-sed 's/\<receiver android/\<!--\<receiver android/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
+#sed 's/\<receiver android:name=\"org.able.capled/\<!--\<receiver android:name=\"org.able.capled/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
 
-sed 's/\<\/receiver\>/\<\/receiver\>--\>/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
+#sed 's/\<\/receiver\>/\<\/receiver\>--\>/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
 
 echo SED done.
 
@@ -17,10 +17,10 @@ echo ABLE Project zipped!
 
 echo Starting Recovery...
 
-sed 's/\<!--\<activity android:name=\"de.tudarmstadt.es.capled.CapLEDActivity\" \/\>--\>/<activity android:name=\"de.tudarmstadt.es.capled.CapLEDActivity\" \/\>/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
+#sed 's/\<!--\<activity android:name=\"org.able.capled.CapLEDActivity\" \/\>--\>/<activity android:name=\"org.able.capled.CapLEDActivity\" \/\>/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
 
-sed 's/\<!--\<receiver android/\<receiver android/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
+#sed 's/\<!--\<receiver android/\<receiver android/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
 
-sed 's/\<\/receiver\>--\>/\<\/receiver\>/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
+#sed 's/\<\/receiver\>--\>/\<\/receiver\>/' ../android/Application/src/main/AndroidManifest.xml > ../android/Application/src/main/AndroidManifest2.xml && mv ../android/Application/src/main/AndroidManifest2.xml ../android/Application/src/main/AndroidManifest.xml
 
 echo Recovery done. Finished!
